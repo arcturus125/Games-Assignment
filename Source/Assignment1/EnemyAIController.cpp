7df0a7a75b5a284c4405ba08e389da09c63e31ce
arcturus125 <arcturus1259@gmail.com>
2021-02-06 16:34:21 +0000

    Upload project to Github

---
+++ Source/Assignment1/EnemyAIController.cpp
@@ -0,0 +1,66 @@
+
+#include "EnemyAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/TargetPoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+
+void AEnemyAIController::BeginPlay() {
+	Super::BeginPlay();
+
+	// get all gameobjects of type TargetPoint and store them in the Waypoints Array
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATargetPoint::StaticClass(), Waypoints);
+
+	for (AActor* Waypoint : Waypoints)
+	{
+		if (Waypoint->ActorHasTag(TEXT("Lookout")))
+		{
+			lookoutPoint = Waypoint;
+
+			GetBlackboardComponent()->SetValueAsVector(TEXT("LookoutPosition"), lookoutPoint->GetActorLocation());
+		}
+	}
+
+	// ##pathfind the enemy to the player
+	//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	//SetFocus(PlayerPawn);
+	//MoveToActor(PlayerPawn, 10.0f); 
+
+	// move to waypoint 1
+	//MoveToActor(Waypoints[0]); 
+
+	//RandomPatrol();
+
+	PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (EnemyBehaviorTree != nullptr)
+	{
+		RunBehaviorTree(EnemyBehaviorTree);
+	}
+	GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerPosition"), PlayerPawn->GetActorLocation());
+
+}
+void AEnemyAIController::Tick(float DeltaTime) 
+{
+	Super::Tick(DeltaTime);
+	GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerPosition"), PlayerPawn->GetActorLocation());
+}
+
+AActor* AEnemyAIController::ChooseWaypoint()
+{
+	int index = FMath::RandRange(0, Waypoints.Num() - 1);
+	return Waypoints[index];
+}
+
+void AEnemyAIController::RandomPatrol()
+{
+
+	MoveToActor(ChooseWaypoint()); 
+}
+
+void AEnemyAIController::OnMoveCompleted(FAIRequestID RequestID, const
+	FPathFollowingResult& Result)
+{
+	Super::OnMoveCompleted(RequestID, Result);
+	//RandomPatrol();
+}
---
+++ Source/Assignment1/EnemyAIController.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENT1_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+private:
+	UPROPERTY()
+	TArray<AActor*> Waypoints;
+	UPROPERTY()
+		AActor* lookoutPoint;
+	UPROPERTY(EditAnywhere)
+		UBehaviorTree* EnemyBehaviorTree;
+	AActor* ChooseWaypoint();
+	void RandomPatrol();
+	APawn* PlayerPawn;
+public:
+	void OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result) override;
+};
